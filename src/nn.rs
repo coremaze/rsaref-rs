8a4eecb87e8993b49f46dbf6c1734f260010e175
ChrisMiuchiz <chris000005@gmail.com>
2022-06-20 14:20:26 -0400

    Port NN_Encode

--- src/nn.rs
+++ src/nn.rs
@@ -34,6 +34,31 @@ impl NNDigit {
 
         result
     }
+
+    /* Encodes digits into character string result, where character string is ordered
+   from most to least significant. */
+    pub fn encode(digits: &[Self]) -> Vec<u8> {
+        let mut result = Vec::<u8>::new();
+        result.reserve(digits.len() * (u32::BITS / u8::BITS) as usize);
+
+        for digit in digits.iter().rev() {
+            let mut n = digit.n;
+            for _ in 0..(u32::BITS / u8::BITS) {
+                // Get uppermost byte of n
+                let top_byte = n & ((u8::MAX as u32) << (u32::BITS - u8::BITS));
+
+                // move the uppermost byte down the the least significant byte
+                let byte = ((top_byte >> (u32::BITS - u8::BITS)) % (u8::MAX as u32 + 1)) as u8;
+
+                // Move the next byte in n to the top
+                n <<= u8::BITS;
+
+                result.push(byte);
+            }
+        }
+
+        result
+    }
 }
 
 impl Default for NNDigit {
@@ -76,5 +101,29 @@ mod tests {
         let digits = NNDigit::decode(&bytes);
 
         assert_eq!(digits.cmp(&correct_digits), Ordering::Equal);
+    }
+
+    #[test]
+    pub fn test_encode1() {
+        let original_digits = vec![
+            NNDigit::new(117967114),
+            NNDigit::new(50595078),
+            NNDigit::new(258),
+        ];
+
+        let encoded_bytes = NNDigit::encode(&original_digits);
+        let decoded_digits = NNDigit::decode(&encoded_bytes);
+
+        assert_eq!(original_digits.cmp(&decoded_digits), Ordering::Equal);
+    }
+
+    #[test]
+    pub fn test_encode2() {
+        let original_digits = vec![NNDigit::new(84281096), NNDigit::new(16909060)];
+
+        let encoded_bytes = NNDigit::encode(&original_digits);
+        let decoded_digits = NNDigit::decode(&encoded_bytes);
+
+        assert_eq!(original_digits.cmp(&decoded_digits), Ordering::Equal);
     }
 }
